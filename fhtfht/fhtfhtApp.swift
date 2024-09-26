4f27453b7b816a9f542d7972899f3da762b0d217
joud <joud13adb@gmail.com>
2024-09-26 17:20:14 +0300

    Initial Commit

---
+++ fhtfht/fhtfhtApp.swift
@@ -0,0 +1,17 @@
+//
+//  fhtfhtApp.swift
+//  fhtfht
+//
+//  Created by Joud Abdullah Alayaqub on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct fhtfhtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fhtfhtTests/fhtfhtTests.swift
@@ -0,0 +1,17 @@
+//
+//  fhtfhtTests.swift
+//  fhtfhtTests
+//
+//  Created by Joud Abdullah Alayaqub on 23/03/1446 AH.
+//
+
+import Testing
+@testable import fhtfht
+
+struct fhtfhtTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
